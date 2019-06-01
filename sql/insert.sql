138be3250a2c0d2d36f2527f23076cfbad41288f
Salas123 <salasii.jesus@gmail.com>
2019-06-01 00:55:56 -0700

    added all files

---
+++ sql/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO Plane (id, make, model, age_YEAR_1970, seats _SEATS) VALUES ('input1','input2','input3','input4','input5');
+
+
+INSERT INTO Pilot (id, fullname, nationality) VALUES ('input1','input2','input3');
+
+
+INSERT INTO Flight (fnum, cost _PINTEGER, num_sold _PZEROINTEGER, num_stops _PZEROINTEGER, actual_departure_date, actual_arrival_date, arrival_airport, departure_airport) VALUES ('input1','input2','input3','input4','input5','input6','input7', 'input8');
+
+
+INSERT INTO Technician (id, fullname) VALUES ('input1','input2');
+
